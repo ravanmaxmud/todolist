821444f60d267a656f23187094b9e91bfe8c476e
ravanmaxmud <ravanim@code.edu.az>
2022-09-26 13:50:03 +0400

    aded

---
+++ app.js
@@ -0,0 +1,23 @@
+function Add() {
+    var value = document.querySelector('#inp').value
+
+    var div = document.createElement('div')
+    div.setAttribute('class', 'text-light bg-dark d-flex justify-content-between align-items-center rounded-2 p-2 mt-3');
+    var h2 = document.createElement('h2');
+    var icon = document.createElement('i');
+    icon.setAttribute('class', 'fa-solid fa-trash-can remove');
+   
+    
+    h2.innerHTML = value
+    div.appendChild(h2)
+    div.appendChild(icon)
+    document.getElementById('first').appendChild(div)
+    var remover =document.querySelectorAll('.remove')
+    console.log(remover)
+    for (var i = 0; i <remover.length; i++) {
+            remover[i].onclick = function () {
+            this.parentElement.remove()
+       
+        }
+    }
+}
